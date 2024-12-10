2703033927eb6a7f108e515a985f1544dfdcdb99
BugHunter-X <askzenai@gmail.com>
2024-12-11 00:24:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,29 @@
+```javascript
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* This route will never match because it's nested under a route with a parameter */}
+        <Route path="/:id" element={<User />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() {
+  return <h1>Home</h1>;
+}
+
+function About() {
+  return <h1>About</h1>;
+}
+
+function User() {
+  return <h1>User</h1>;
+}
+export default App; 
+```
